b686eee943a214fbb7982088f811ba6870d9f251
Julius Babies <julius@familie-babies.de>
2020-09-23 17:37:51 +0200

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World. What's your name?");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello " + sc.next());
+    }
+}
